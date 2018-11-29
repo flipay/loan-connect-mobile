5cf4ac921cc9381dbe7b285d24def1fcab03fee4
Siwakorn Thitisaksakul <kendojaa@Siwakorns-MacBook-Pro.local>
2018-11-29 23:10:04 +0700

    :bento: add color codes for the project

---
+++ src/constants/styleGuide.ts
@@ -0,0 +1,21 @@
+
+export const COLORS = {
+  P100: '#DAD1FF',
+  P200: '#B4A2FE',
+  P300: '#9177FE',
+  P400: '#7D5EFE',
+  P500: '#6140BE',
+  C100: '#CCFFFF',
+  C200: '#B2FFFF',
+  C300: '#7FFFFF',
+  C400: '#02ECFF',
+  C500: '#03D8EA',
+  N100: '#F4F5F7',
+  N200: '#EDEFF3',
+  N300: '#DFE3EB',
+  N400: '#B4BCCA',
+  N500: '#8993A4',
+  N600: '#5D687F',
+  N700: '#3C4558',
+  N800: '#091E42'
+}
