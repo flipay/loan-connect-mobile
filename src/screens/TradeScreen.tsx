cfdafda752693b31392c7bd89fe216e3f5fecb6f
Siwakorn Thitisaksakul <kendojaa@Siwakorns-MacBook-Pro.local>
2019-01-10 15:13:28 +0700

    :lipstick: have the header

--- src/screens/TradeScreen.tsx
+++ src/screens/TradeScreen.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
-import { Text } from 'react-native'
+import { ScrollView, StyleSheet, View } from 'react-native'
 import { NavigationScreenProps } from 'react-navigation'
-import { ScrollView } from 'react-native'
+import { FontAwesome } from '@expo/vector-icons'
+import { Text } from '../components'
 
 export default class TradeScreen extends React.Component<
   NavigationScreenProps
@@ -15,6 +16,27 @@ export default class TradeScreen extends React.Component<
     }
   }
 
+  public renderCloseButton () {
+    return (
+      <View style={styles.closeButtonContainer}>
+        <FontAwesome name='times' size={16} />
+      </View>
+    )
+  }
+
+  public renderHeader () {
+    return (
+      <View style={styles.headerContainer}>
+        <Text type='title'>
+          {`${this.props.navigation.getParam('side')} ${this.props.navigation.getParam('coinId')}`}
+        </Text>
+        <Text type='body'>
+          3,000 THB available
+        </Text>
+      </View>
+    )
+  }
+
   public render () {
     return (
       <ScrollView
@@ -23,8 +45,19 @@ export default class TradeScreen extends React.Component<
           flex: 1
         }}
       >
-        <Text>buy / sell</Text>
+        {this.renderCloseButton()}
+        {this.renderHeader()}
       </ScrollView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  closeButtonContainer: {
+    alignContent: 'flex-start',
+    marginLeft: 18
+  },
+  headerContainer: {
+    alignItems: 'center'
+  }
+})
