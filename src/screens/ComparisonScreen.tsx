eb0b40b7d11a5719ae9c76a418b0287dad7afc1b
Siwakorn Thitisaksakul <clever_buffalo@hotmail.com>
2019-01-14 16:28:36 +0700

    :sparkles: finish the styling of comparison page

--- src/screens/ComparisonScreen.tsx
+++ src/screens/ComparisonScreen.tsx
@@ -3,14 +3,13 @@ import * as _ from 'lodash'
 import {
   View,
   StyleSheet,
-  ScrollView,
   StatusBar
 } from 'react-native'
 import { NavigationScreenProps } from 'react-navigation'
-import { Constants } from 'expo'
+import { LinearGradient } from 'expo'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { Text, CloseButton } from '../components'
 import { COLORS } from '../constants/styleGuides'
-import { ASSETS } from '../constants/assets'
 
 interface State {
   activeTradeBoxIndex: number
@@ -39,13 +38,20 @@ export default class TradeScreen extends React.Component<
     this.props.navigation.goBack()
   }
 
-  public renderRecord (data: Record) {
+  public renderRecord (data: Record, index: number) {
     return (
-      <View style={styles.record}>
+      <View style={styles.tableRecord} key={data.name}>
         <Text>{data.name}</Text>
         <View style={styles.rightPartRecord}>
           <Text>{data.amount}</Text>
-          <Text>{data.difference}</Text>
+          {index === 0 ? (
+            <Text type='caption' color={COLORS.N500}>Best Price</Text>
+          ) : (
+            <View style={styles.captionRow}>
+              <MaterialCommunityIcons name='trending-down' color='#FE4747' style={styles.downTrendIcon} />
+              <Text type='caption' color={COLORS.N500}>{`- ${data.difference}`}</Text>
+            </View>
+          )}
         </View>
       </View>
     )
@@ -73,14 +79,18 @@ export default class TradeScreen extends React.Component<
 
     return (
       <View>
-        {mockData.map((data) => this.renderRecord(data))}
+        {mockData.map((data, index) => this.renderRecord(data, index))}
       </View>
     )
   }
 
   public renderTable () {
     return (
-      <View>
+      <View style={styles.table}>
+        <View style={styles.header}>
+          <Text color={COLORS.N500}>If you use</Text>
+          <Text color={COLORS.N500}>You will receive</Text>
+        </View>
         {this.renderTableBody()}
       </View>
     )
@@ -88,14 +98,19 @@ export default class TradeScreen extends React.Component<
 
   public render () {
     return (
-      <View style={styles.container}>
+      <LinearGradient
+        colors={[COLORS.P400, COLORS.C500]}
+        start={[0.3, 0.7]}
+        end={[2, -0.8]}
+        style={styles.container}
+      >
         <StatusBar barStyle='light-content' />
         <CloseButton onPress={this.onClose} color={COLORS.WHITE} />
         <Text type='title' color={COLORS.WHITE}>Save 500 THB with us!</Text>
         <Text color={COLORS.WHITE}>Looks like Flipay is the cheapest way to buy</Text>
         <Text color={COLORS.WHITE}>Bitcoin with 1,000 THB</Text>
         {this.renderTable()}
-      </View>
+      </LinearGradient>
     )
   }
 }
@@ -103,16 +118,44 @@ export default class TradeScreen extends React.Component<
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 40,
+    paddingTop: 86,
+    paddingHorizontal: 16,
     alignItems: 'center',
     backgroundColor: 'blue',
     position: 'relative'
   },
-  record: {
-    flexDirection: 'row'
+  table: {
+    backgroundColor: COLORS.WHITE,
+    padding: 16,
+    marginTop: 24,
+    width: '100%',
+    borderRadius: 4
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 16,
+    paddingHorizontal: 8
+  },
+  tableRecord: {
+    borderTopColor: COLORS.N200,
+    borderTopWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: 16,
+    paddingRight: 8,
+    paddingVertical: 24
   },
   rightPartRecord: {
     justifyContent: 'center',
     alignItems: 'flex-end'
+  },
+  captionRow: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  downTrendIcon: {
+    marginRight: 4
   }
 })
