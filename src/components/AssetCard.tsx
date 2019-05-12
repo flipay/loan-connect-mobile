2d41d91366f96269c391dda5efe75256edcc9443
Siwakorn Thitisaksakul <clever_buffalo@hotmail.com>
2019-05-12 14:32:17 +0700

    :lipstick: change the position of the TransferButton

--- src/components/AssetCard.tsx
+++ src/components/AssetCard.tsx
@@ -105,12 +105,9 @@ export default class AssetCard extends React.Component<
       </Value>
     ) : (
       <View style={styles.coinMainContent}>
-        <View style={{ flexDirection: 'row' }}>
-          <Value assetId={this.props.id} fontType='title'>
-            {this.props.amount}
-          </Value>
-          {this.renderTransferButton()}
-        </View>
+        <Value assetId={this.props.id} fontType='title'>
+          {this.props.amount}
+        </Value>
         <Value assetId='THB' fontType='body'>
           {(this.props.price || 0) * this.props.amount}
         </Value>
@@ -138,7 +135,7 @@ export default class AssetCard extends React.Component<
   public renderTransferButton () {
     return (
       <TouchableOpacity onPress={this.props.onPressTranferButton} style={styles.transferButton}>
-        <FontAwesome name='exchange' color={COLORS.P200} />
+        <FontAwesome name='exchange' color={COLORS.P400} />
       </TouchableOpacity>
     )
   }
@@ -158,6 +155,7 @@ export default class AssetCard extends React.Component<
               this.props.expanded && styles.expandedContainer
             ]}
           >
+            {this.props.expanded && this.renderTransferButton()}
             <Asset id={this.props.id} />
             {!this.props.expanded && (
               <View style={styles.rightSection}>
@@ -189,6 +187,8 @@ export default class AssetCard extends React.Component<
   }
 }
 
+const paddingLeft = 20
+
 const styles = StyleSheet.create({
   expandedContainer: {
     flexDirection: 'column',
@@ -197,7 +197,7 @@ const styles = StyleSheet.create({
   },
   container: {
     height: '100%',
-    padding: 20,
+    padding: paddingLeft,
     alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'space-between'
@@ -216,12 +216,12 @@ const styles = StyleSheet.create({
   },
   transferButton: {
     position: 'absolute',
-    right: -40,
-    marginLeft: 10,
+    left: paddingLeft,
+    top: 20,
     backgroundColor: COLORS.N200,
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 15,
+    borderRadius: 4,
     width: 30,
     height: 30
   },
