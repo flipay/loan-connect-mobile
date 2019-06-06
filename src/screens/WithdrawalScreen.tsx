0e936822a427c4369bdc46256c0a2e7ec1ebd553
Siwakorn Thitisaksakul <clever_buffalo@hotmail.com>
2019-06-06 12:52:39 +0700

    :piggyback: :bug: fixing the duplicate header in Withdrawal Result screen

--- src/screens/WithdrawalScreen.tsx
+++ src/screens/WithdrawalScreen.tsx
@@ -154,7 +154,6 @@ export default class WithdrawalScreen extends React.Component<
     const addressType = assetId === 'THB' ? 'bank account' : `${ASSETS[assetId].name} address`
     return (
       <View style={styles.resultContainer}>
-        <Text type='title' style={styles.header}>{`Withdrawal ${ASSETS[assetId].name}`}</Text>
         <Text>{`We’ll transfer to your ${addressType} within 24 hours. We may reach out to you by phone if we need more information.`}</Text>
       </View>
     )
