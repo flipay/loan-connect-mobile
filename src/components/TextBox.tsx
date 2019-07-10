4bf6865e0625b660611a67fd2f8bff914742b507
Siwakorn Thitisaksakul <clever_buffalo@hotmail.com>
2019-07-10 12:47:06 +0700

    :lipstick: improve TextBox

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -10,7 +10,7 @@ import { COLORS, FONT_TYPES } from '../constants'
 
 interface Props {
   autoFocus?: boolean
-  description: string
+  label: string
   placeholder?: string
   onPress: () => void
   onChangeValue: (value: string) => void
@@ -37,24 +37,22 @@ export default class TextBox extends React.Component<Props> {
         onPress={this.onPress}
         active={this.props.active}
       >
-        <View style={styles.leftContainer}>
-          <Text type='caption' color={this.props.active ? COLORS.P400 : COLORS.N500}>{this.props.description}</Text>
-          <TextInput
-            ref={element => {
-              this.input = element
-            }}
-            style={styles.textInput}
-            autoFocus={this.props.autoFocus}
-            placeholder={this.props.placeholder}
-            placeholderTextColor={this.props.active ? COLORS.P100 : COLORS.N300}
-            selectionColor={COLORS.P400}
-            onChangeText={text => this.props.onChangeValue(text)}
-            value={this.props.value}
-            keyboardType={this.props.numberPad ? 'decimal-pad' : 'default'}
-            onFocus={this.props.onPress}
-            autoCorrect={this.props.autoCorrect}
-          />
-        </View>
+        <Text type='caption' color={this.props.active ? COLORS.P400 : COLORS.N500}>{this.props.label}</Text>
+        <TextInput
+          ref={element => {
+            this.input = element
+          }}
+          style={styles.textInput}
+          autoFocus={this.props.autoFocus}
+          placeholder={this.props.placeholder}
+          placeholderTextColor={this.props.active ? COLORS.P100 : COLORS.N300}
+          selectionColor={COLORS.P400}
+          onChangeText={text => this.props.onChangeValue(text)}
+          value={this.props.value}
+          keyboardType={this.props.numberPad ? 'decimal-pad' : 'default'}
+          onFocus={this.props.onPress}
+          autoCorrect={this.props.autoCorrect}
+        />
       </Layer>
     )
   }
@@ -62,24 +60,12 @@ export default class TextBox extends React.Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row'
-  },
-  leftContainer: {
-    flex: 2,
+    flex: 1,
     padding: 10
   },
   textInput: {
-    fontSize: FONT_TYPES['large-title'].fontSize,
+    fontSize: FONT_TYPES['body'].fontSize,
     fontFamily: FONT_TYPES['large-title'].fontFamily,
     color: COLORS.N800
-  },
-  rightContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    borderLeftWidth: 1,
-    borderLeftColor: COLORS.N200,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    paddingLeft: 16
   }
 })
