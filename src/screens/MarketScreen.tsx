7cf3386f8dd939a70a56660f521327fb8f1fdf12
Siwakorn Thitisaksakul <clever_buffalo@hotmail.com>
2019-08-12 11:16:30 +0700

    :fire: remove unused poc

--- src/screens/MarketScreen.tsx
+++ src/screens/MarketScreen.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import _ from 'lodash'
 import { View, StyleSheet, Image, TouchableOpacity } from 'react-native'
-import { WebView } from 'react-native-webview'
 import { NavigationScreenProps } from 'react-navigation'
 import { showPrice } from '../utils'
 import { Text, Layer, ChangeBox, ScreenWithCover } from '../components'
@@ -110,7 +109,6 @@ export default class MarketScreen extends React.Component<Props & NavigationScre
       <View style={styles.headerContent}>
         <Text type='large-title' bold={true} color={COLORS.WHITE}>Market</Text>
         <Text color={COLORS.WHITE}>See what's going on in crypto market</Text>
-        <WebView source={{ uri: 'https://flipay-charts.firebaseapp.com/' }} style={{ marginTop: 20 }} />
       </View>
     )
   }
@@ -130,8 +128,7 @@ export default class MarketScreen extends React.Component<Props & NavigationScre
 
 const styles = StyleSheet.create({
   headerContent: {
-    marginBottom: 24,
-    height: 500
+    marginBottom: 24
   },
   cryptoContainer: {
   },
